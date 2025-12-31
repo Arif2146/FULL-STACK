d251a56c6b28e987e7dfb4a6430bedc5ec7d2258
Arif Bepari <arifbepari2006@gmail.com>
2025-12-31 15:23:57 +0530

    create and finished Tags controller

---
+++ BACK-END/controllers/Tags.js
@@ -0,0 +1,25 @@
+const Tag = require('../models/Tag');
+
+
+exports.createTag = async (req, res) => {
+    try {
+       const { name,description } = req.body;
+       if(!name || !description){
+        return res.status(400).json({ message: "Name and Description are required", success: false });
+       }
+       const tagDetails = await Tag.create({ name: name, description: description });
+       console.log(tagDetails);
+       res.status(201).json({ message: "Tag created successfully", success: true });
+} catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }
+}    
+exports.getAllTags = async (req, res) => {
+    try {
+        const allTags = await Tag.find({},{name:true,description:true});
+        res.status(200).json({ message: "Tags fetched successfully", success: true,allTags });
+
+    }catch (error) {
+        res.status(500).json({ message: error.message, success: false });
+    }    
+}
